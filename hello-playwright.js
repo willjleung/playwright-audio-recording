01785a58768598520a20ab608f772bff508474ac
willjleung <willjleung@gmail.com>
2024-10-31 04:06:33 -0400

    10/31/24 Update
    Working on recordings (currently unsucessful)

--- hello-playwright.js
+++ hello-playwright.js
@@ -4,60 +4,78 @@ const { firefox } = require('playwright');
   //Opening Browser
   const browser = await firefox.launch({headless: false});
   
-  //Opening Context and Setting up video recording
+  //Opening Context 
+  //Commented paramater can record video of the test (no audio)
   const context = await browser.newContext({/*recordVideo: {dir:'videos'}*/});
 
   //Opening Page
   const page = await context.newPage();
 
-
-  const CONTEXT = await page.evaluate(()=>{
-    //Testing Log
-    //return "start of logtest";
-
+  //page.evaluate is used to access the AudioContext in the PlayWright Automation
+  await page.evaluate(()=>{
+    //Functions for Recording
+    //startrecording takes in an Audio Context as a parameter
     function startrecording(ctx){
-      recordingstream=ctx.createMediaStreamDestination();
-      recorder=new MediaRecorder(recordingstream.stream);
+      //Creating a recording stream
+      recordingstream = ctx.createMediaStreamDestination();
+      //Creating a Media Recorder with the correct stream
+      recorder = new MediaRecorder(recordingstream.stream);
       recorder.start();
     }
-    
     function stoprecording(){
       recorder.addEventListener('dataavailable',function(e){
         document.querySelector('#recording').src=URL.createObjectURL(e.data);
-        recorder=false;
-        recordingstream=false;
+        recorder = false;
+        recordingstream = false;
       });
       recorder.stop();
     }
-    
+
+    //Creating Constants
+    const audioChunks = []
+    bufferSize = 4096;
+
+    //Creating Media Stream for speaker
+    var speaker = new MediaStream;
+
     //Creating Audio Context
     const ctx = new AudioContext();
+
+    //Adding audio tracks to stream
+//    speaker.addTrack(recorder.stream)
 
     //Playing Sound
     const osc = ctx.createOscillator(500);
     osc.connect(ctx.destination);
     osc.start();
 
+
+
+    //Creating Audio Recording
+    const source = ctx.createMediaStreamSource(speaker);
+    const processor = ctx.createScriptProcessor(bufferSize, 1, 1);
+
     //Starting Audio Recording
     startrecording(ctx);
     //const mediaRecorder = new MediaRecorder(stream);
-    
+
+    //Stop Recording    
     stoprecording();
 
-    //Tried to return Audio Context | Does not work ATM
-    return ctx;
+    //const blob = new Blob([wav], {type: 'audio/wav'});
+    //const url = URL.createObjectURL(blob);
+    //url.click();
 
     //Testing Log
     //return "End of logtest";
   })
-  //startrecording();
-  //console.log(logTest);
-
   //Opening test playground
   await page.goto('https://playground.babylonjs.com/#SPLFNK#1');
   console.log("Webpage Loaded");
-  //stoprecording();
 
+
+  //Screenshot
+  //await page.screenshot({ path: 'screenshot.png' });
 
   //Closing Browsers
   await context.close();
